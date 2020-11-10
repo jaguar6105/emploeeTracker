d13c953b90308ecbf34946d104a395a8ea36aa34
jaguar6105 <gilljoseph603@gmail.com>
2020-11-10 16:11:35 -0500

    add all view functions and finish switch statement

--- schema.sql
+++ schema.sql
@@ -5,13 +5,13 @@ USE employeeManagerDb;
 
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     department VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function mainMenu() {
     try {
         const { answer } = await inquirer.prompt({
             type: "list",
-            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager","Add an Employee","Remove an Employee","Update Employee Role","Update Employee Manager", "View All Roles", "View All Departments","Exit"],
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add an Employee", "Remove an Employee", "Update Employee Role", "Update Employee Manager", "View All Roles", "View All Departments", "Exit"],
             name: "answer",
             message: "What do would you like to do:"
         });
@@ -29,12 +29,86 @@ async function mainMenu() {
     }
 }
 
+async function viewEmployeeByDep() {
+
+
+    connection.query("SELECT * FROM employee ORDER BY role_id", function (err, res) {
+        if (err) throw err;
+        console.log("TEST");
+        console.log(res);
+    });
+}
+
+async function viewEmployeeByMan() {
+
+
+    connection.query("SELECT * FROM employee ORDER BY manager_id", function (err, res) {
+        if (err) throw err;
+        console.log("TEST");
+        console.log(res);
+    });
+}
+
+async function viewEmployee() {
+
+
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.log("TEST");
+        console.log(res);
+    });
+}
+
+async function viewRoles() {
+    connection.query("SELECT * FROM roles", function (err, res) {
+        if (err) throw err;
+        console.log("TEST");
+        console.log(res);
+    });
+}
+
+async function viewDepartments() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        console.log("TEST");
+        console.log(res);
+    });
+}
 
 
 async function init() {
     var response = "";
     while (response != "Exit") {
         response = await mainMenu();
+        switch (response) {
+            case "View All Employees":
+                viewEmployee();
+                break;
+            case "View All Employees By Department":
+                viewEmployeeByDep();
+                break;
+            case "View All Employees By Manager":
+                viewEmployeeByMan();
+                break;
+            case "Add an Employee":
+                //viewEmployee();
+                break;
+            case "Remove an Employee":
+                //viewEmployeeByDep();
+                break;
+            case "Update Employee Role":
+                //viewEmployeeByMan();
+                break;
+            case "Update Employee Manager":
+                //viewEmployee();
+                break;
+            case "View All Roles":
+                viewRoles();
+                break;
+            case "View All Departments":
+                viewDepartments();
+                break;
+        }
     }
     connection.end();
 }
