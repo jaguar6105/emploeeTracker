f9cb381e951a69046293262d03ce25567b538fc2
jaguar6105 <gilljoseph603@gmail.com>
2020-11-10 14:51:04 -0500

    add main question loop

--- script.js
+++ script.js
@@ -13,5 +13,30 @@ const connection = sql.createConnection({
     database: "employeeManagerDb"
 });
 
+//initial question
+async function mainMenu() {
+    try {
+        const { answer } = await inquirer.prompt({
+            type: "list",
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager","Add an Employee","Remove an Employee","Update Employee Role","Update Employee Manager", "View All Roles", "View All Departments","Exit"],
+            name: "answer",
+            message: "What do would you like to do:"
+        });
+        return answer;
 
-connection.end();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+
+async function init() {
+    var response = "";
+    while (response != "Exit") {
+        response = await mainMenu();
+    }
+    connection.end();
+}
+
+init();
